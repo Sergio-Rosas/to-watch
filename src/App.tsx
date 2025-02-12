2d8135617d9af5d15e95c6dc2f37a5a2caf70e3b
Sergio-Rosas <75125177+Sergio-Rosas@users.noreply.github.com>
2025-02-12 11:57:49 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ function App() {
 
   return (
           <section>
-
-            <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP2SMDS"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-            
               <Movies/>
           </section>
   )
