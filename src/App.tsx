b167f831561ce72172c4839175d3f84183ce674b
Sergio-Rosas <75125177+Sergio-Rosas@users.noreply.github.com>
2025-02-12 11:50:37 -0500

    Update App.tsx putting the GTN noscript

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
           <section>
+
+            <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TP2SMDS"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+            
               <Movies/>
           </section>
   )
